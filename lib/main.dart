8ebe5fa06c8c1ae949424f2aa594b9a6c7c8eae1
CNCSAndroid <nlcompsolutions@gmail.com>
2020-10-11 10:18:32 +0530

    Added new functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopapp/screens/product_details_screen.dart';
+import './screens/cart_screen.dart';
+import './providers/Cart.dart';
+import './screens/product_details_screen.dart';
 import './providers/products_provider.dart';
 import './screens/product_overview_screen.dart';
 
@@ -12,9 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      //builder: (ctx) => Products(),
-      value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
@@ -25,6 +33,7 @@ class MyApp extends StatelessWidget {
         home: ProductOverviewScreen(),
         routes: {
           ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
         },
       ),
     );
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../screens/cart_screen.dart';
+import '../widgets/badge.dart';
+import '../providers/products_provider.dart';
 import '../widgets/products_grid.dart';
+import '../providers/Cart.dart';
 
 enum FilterOptions { Favourites, All }
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: <Widget>[
           PopupMenuButton(
             onSelected: (FilterOptions selectedValue) {
-              print(selectedValue.toString());
+              if (selectedValue == FilterOptions.Favourites) {
+                productContainer.showFavouritesOnly();
+              } else {
+                productContainer.showAll();
+              }
             },
             icon: Icon(
               Icons.more_vert,
@@ -24,6 +34,20 @@ class ProductOverviewScreen extends StatelessWidget {
               PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
             ],
           ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: (){
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          )
         ],
       ),
       body: ProductsGrid(),
